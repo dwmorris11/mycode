38fbed98d4488cb184e04043b20f173838c93590
Dustin Morris <dustinnuc@gmail.com>
2021-01-04 21:57:48 +0000

    manipulating lists

---
+++ challenge.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+user_input_name = input("What is your name? ")
+user_input_day = input("What is the day? ")
+
+print("Hello,", user_input_name + "!", "Happy", user_input_day + "!")
+
